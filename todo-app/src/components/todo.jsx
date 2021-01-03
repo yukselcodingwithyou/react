adea2b66929d2dfa32842e5fa1933d56b64fa71e
yuksel.ozdemir@tokeninc.com <yuksel.ozdemir@tokeninc.com>
2021-01-03 10:41:23 +0300

    01-added initial version w/ material-ui

---
+++ todo-app/src/components/todo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class Todo extends Component {
+    render() {
+        return ( <Typography variant="h5" component="h2" color="primary">
+                            Hello World!
+                 </Typography>
+                );
+    }
+}
+
+export default Todo;
---
+++ todo-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todo from './components/todo';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
